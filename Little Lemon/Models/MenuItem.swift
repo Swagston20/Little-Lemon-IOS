da153142499a775e20569704b29bccc442e967da
Swagston20 <88470259+Swagston20@users.noreply.github.com>
2023-02-25 10:49:32 +0800

    Add files via upload

---
+++ Little Lemon/Models/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Little Lemon
+//
+//  Created by Mike on 2/23/23.
+//
+
+struct MenuItem: Decodable {
+    let title: String
+    let image: String
+    let price: String
+    let description: String
+    let category: String
+}
---
+++ Little Lemon/Models/MenuList.swift
@@ -0,0 +1,10 @@
+//
+//  MenuList.swift
+//  Little Lemon
+//
+//  Created by Mike on 2/23/23.
+//
+
+struct MenuList: Decodable {
+    let menu: [MenuItem]
+}
